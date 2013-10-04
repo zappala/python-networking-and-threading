481c5c98b89f055e5544dc32a2343202bd7e88d1
Daniel Zappala <daniel.zappala@gmail.com>
2013-10-04 11:50:20 -0600

    Simple threading examples.

---
+++ threadHello.py
@@ -0,0 +1,30 @@
+import argparse
+import sys
+import threading
+
+class Hello(threading.Thread):
+    """ A thread that says hello. """
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        print "Hello from thread",self.getName()
+
+
+
+def parse_options():
+        parser = argparse.ArgumentParser(prog='threadHello', description='Simple demonstration of threading', add_help=True)
+        parser.add_argument('-n', '--number', type=int, action='store', help='Specify the number of threads to create')
+        return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_options()
+    threads = []
+    for i in range(0,args.number):
+        h = Hello()
+        threads.append(h)
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
---
+++ threadShared.py
@@ -0,0 +1,44 @@
+import argparse
+import sys
+import threading
+
+class Shared:
+    """ Shared memory """
+    def __init__(self):
+        self.sharedi = 0
+        self.sem = threading.Semaphore()
+        self.lock = threading.Lock()
+
+    def inc(self):
+        """ increment the shared varable """
+        self.sem.acquire()
+        self.sharedi = self.sharedi + 1
+        s = self.sharedi
+        self.sem.release()
+        return s
+
+class Hello(threading.Thread):
+    """ A thread that increments and prints both a local and shared
+    variable """
+    def __init__(self,shared):
+        threading.Thread.__init__(self)
+        self.i = 0
+        self.shared = shared
+
+    def run(self):
+        self.i = self.i + 1
+        s = self.shared.inc()
+        with self.shared.lock:
+            print "Hello from thread %s i = %d shared = %d" % (self.getName(),i,s)
+
+def parse_options():
+        parser = argparse.ArgumentParser(prog='threadHello', description='Simple demonstration of threading', add_help=True)
+        parser.add_argument('-n', '--number', type=int, action='store', help='Specify the number of threads to create')
+        return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_options()
+    s = Shared()
+    for i in range(0,args.number):
+        h = Hello(s)
+        h.start()
